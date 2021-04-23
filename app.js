67a2c74cbe7f39e046f1e71a4e658fc7d512b6d5
christianturner <christiandavidphoto@gmail.com>
2021-04-23 15:42:35 -0400

    ejs templates

--- app.js
+++ app.js
@@ -2,31 +2,22 @@ const express = require("express");
 
 const app = express();
 
-// listen for requests
+app.set("view engine", "ejs");
 
-//infers we want localhost
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  // infers the type of contnet we're sending
-  // and sets Content-Type & statusCode for us
-  //   res.send(`<h2>Home Express Test</h2>`);
-
-  // by default Express looks for an absolute path, so if we add a relative path, we add a second arguement which is a path to the root (i.e. the root is what the path is RELATIVE to)
-  res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index");
 });
 app.get("/about", (req, res) => {
-  // res.send(`<h2>About Express Test</h2>`);
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about");
 });
 
-// redirects
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create");
 });
 
 // 404
-// use function fires for every request the server makes, but will only run if it reaches that point (i.e. reaches an error)
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404");
 });
